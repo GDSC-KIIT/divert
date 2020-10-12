b67e1e0b607afb600c35bb68df283f4b028927df
Saswata Mukherjee <saswataminsta@yahoo.com>
2020-10-12 13:49:23 +0530

    Add TypeScript

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,8 +1,11 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
+interface HeaderProps {
+  siteTitle: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ siteTitle }) => (
   <header
     style={{
       background: `rebeccapurple`,
@@ -30,10 +33,6 @@ const Header = ({ siteTitle }) => (
     </div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
 
 Header.defaultProps = {
   siteTitle: ``,
